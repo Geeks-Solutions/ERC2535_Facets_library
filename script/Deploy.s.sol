8526b485e9b20443dfbdba397e433a527609953b
Julien <julien@geeks.solutions>
2024-04-25 10:35:01 +0200

    Diamond and basic Facets addition with deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25 <0.9.0;
 
-import { Foo } from "../src/Foo.sol";
-
 import { BaseScript } from "./Base.s.sol";
+import "../src/ERC2535_Facets_Library.sol";
+import "../src/facets/diamondCutFacet.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcast returns (FacetsLibrary Fl) {
+        DiamondCutFacet Dc = new DiamondCutFacet();
+        Fl = new FacetsLibrary{ salt: "Geeks" }(address(Dc)); // adding a salt to trigger CREATE2
     }
 }
